4b95b608a47dad319c8f7e232ecda432a68ac171
Ferran Olivella <ferry87@gmail.com>
2019-06-08 12:59:43 +0200

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+
+import { APP_ROUTING } from './app.routes';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { PricesComponent } from './components/prices/prices.component';
+import { ProtectedComponent } from './components/protected/protected.component';
+
+
+// Services
+import { AuthService } from './services/auth.service';
+import { AuthGuardService } from './services/auth-guard.service';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    PricesComponent,
+    ProtectedComponent
+  ],
+  imports: [
+    BrowserModule,
+    APP_ROUTING
+  ],
+  providers: [
+    AuthService,
+    AuthGuardService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
